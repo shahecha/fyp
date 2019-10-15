a75e3c3a2471ebfe330bfeeb532fc85000ab5ec6
aimanzaki <aiman0zaki@gmail.com>
2019-10-15 08:26:21 +0800

    Update Gout using custom adapter for nested child

---
+++ app/src/main/java/com/example/foodrecommendation/FoodAdapter.java
@@ -0,0 +1,77 @@
+package com.example.foodrecommendation;
+
+import android.app.Activity;
+import android.content.Context;
+import android.media.Image;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class FoodAdapter extends ArrayAdapter<foodconst> {
+    private Activity activity;
+    private ArrayList<foodconst> foods;
+    private static LayoutInflater inflater = null;
+
+    public FoodAdapter(Activity activity, int textVieweResourceId,ArrayList<foodconst> foods){
+        super(activity,textVieweResourceId,foods);
+        try {
+            this.activity = activity;
+            this.foods = foods;
+            inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+        catch (Exception e){
+
+        }
+    }
+    public  int getCount(){
+        return foods.size();
+    }
+
+    public long getItemId(int index){
+        return index;
+    }
+
+    public  static class ViewHolder{
+        public TextView title;
+        public TextView desc;
+        public ImageView image;
+
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent){
+        View vi = convertView;
+        final ViewHolder holder ;
+
+        try{
+            if(convertView == null){
+                holder = new ViewHolder();
+                vi = inflater.inflate(R.layout.fooddetails,null);
+                holder.title = (TextView) vi.findViewById(R.id.title);
+                holder.desc = (TextView) vi.findViewById(R.id.desc);
+                holder.image = (ImageView) vi.findViewById(R.id.imageView);
+                vi.setTag(holder);
+            }
+            else{
+                holder = (ViewHolder) vi.getTag();
+            }
+
+            holder.title.setText(foods.get(position).getTitle());
+            holder.title.setText(foods.get(position).getDesc());
+            Picasso.get().load(foods.get(position).getImage()).into(holder.image);
+        }
+            catch (Exception e){
+
+        }
+        return  vi;
+    }
+}
--- app/src/main/java/com/example/foodrecommendation/test.java
+++ app/src/main/java/com/example/foodrecommendation/test.java
@@ -8,10 +8,12 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ImageView;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -26,16 +28,20 @@ import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 import com.squareup.picasso.PicassoProvider;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class test extends AppCompatActivity {
 
     Button button;
     ListView lv;
     FirebaseListAdapter adapter;
+    FoodAdapter adapterFood;
     private DatabaseReference query;
 
-    CharSequence[] items={"Fat","Fruits","Vegetable","Animal source food","Nuts","Grains"};
-    boolean[] selectedItems={false,false,false,false,false,false};
+    CharSequence[] items = {"Fat", "Fruits", "Vegetable", "Animal source food", "Nuts", "Grains"};
+    boolean[] selectedItems = {false, false, false, false, false, false};
 
 
     @Override
@@ -43,47 +49,47 @@ public class test extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food);
 
-        button=findViewById(R.id.button);
+        button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                AlertDialog.Builder alertDialogBuilder=new AlertDialog.Builder(test.this);
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(test.this);
                 alertDialogBuilder.setCancelable(true);
                 alertDialogBuilder.setTitle("Select Food Category");
                 alertDialogBuilder.setMultiChoiceItems(items, selectedItems, new DialogInterface.OnMultiChoiceClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int which, boolean isChecked) {
-                        selectedItems[which]=isChecked;
+                        selectedItems[which] = isChecked;
                     }
                 });
                 alertDialogBuilder.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int which) {
                         //letak coding retrieve yang filter
-                        if (selectedItems[0]==true) // display fat only
+                        if (selectedItems[0] == true) // display fat only
                         {
-                            lv=(ListView)findViewById(R.id.listView);
+                            lv = (ListView) findViewById(R.id.listView);
 
-                            final FirebaseListOptions<foodconst>options=new FirebaseListOptions.Builder<foodconst>()
+                            final FirebaseListOptions<foodconst> options = new FirebaseListOptions.Builder<foodconst>()
                                     .setLayout(R.layout.fooddetails)
-                                    .setQuery(query,foodconst.class)
+                                    .setQuery(query, foodconst.class)
                                     .build();
 
-                            adapter=new FirebaseListAdapter (options) {
+                            adapter = new FirebaseListAdapter(options) {
                                 @Override
-                                protected void populateView(@NonNull  View v, @NonNull Object model, int position) {
-                                    final TextView title= v.findViewById(R.id.title);
-                                    final TextView desc=v.findViewById(R.id.desc);
-                                    final ImageView image=v.findViewById(R.id.imageView);
+                                protected void populateView(@NonNull View v, @NonNull Object model, int position) {
+                                    final TextView title = v.findViewById(R.id.title);
+                                    final TextView desc = v.findViewById(R.id.desc);
+                                    final ImageView image = v.findViewById(R.id.imageView);
 
                                     query.child("gout").child("suggested").child("fat").addValueEventListener(new ValueEventListener() {
                                         @Override
                                         public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                                            if(dataSnapshot.getValue()!=null){
+                                            if (dataSnapshot.getValue() != null) {
 
-                                                foodconst fc=dataSnapshot.getValue(foodconst.class);
-                                                title.setText("Title:"+fc.getTitle());
-                                                desc.setText("Decsription:"+fc.getDesc());
+                                                foodconst fc = dataSnapshot.getValue(foodconst.class);
+                                                title.setText("Title:" + fc.getTitle());
+                                                desc.setText("Decsription:" + fc.getDesc());
                                                 Picasso.get().load(fc.getImage()).into(image);
 
                                             }
@@ -94,35 +100,35 @@ public class test extends AppCompatActivity {
                                         public void onCancelled(@NonNull DatabaseError databaseError) {
 
                                         }
-                                    });lv.setAdapter(adapter);
+                                    });
+                                    lv.setAdapter(adapter);
                                 }
                             };
 
-                        }
-                        else if (selectedItems[0]==true && selectedItems[1]==true) //display fat and fruit only
+                        } else if (selectedItems[0] == true && selectedItems[1] == true) //display fat and fruit only
                         {
-                            lv=(ListView)findViewById(R.id.listView);
+                            lv = (ListView) findViewById(R.id.listView);
 
-                            final FirebaseListOptions<foodconst>options=new FirebaseListOptions.Builder<foodconst>()
+                            final FirebaseListOptions<foodconst> options = new FirebaseListOptions.Builder<foodconst>()
                                     .setLayout(R.layout.fooddetails)
-                                    .setQuery(query,foodconst.class)
+                                    .setQuery(query, foodconst.class)
                                     .build();
 
-                            adapter=new FirebaseListAdapter (options) {
+                            adapter = new FirebaseListAdapter(options) {
                                 @Override
-                                protected void populateView(@NonNull  View v, @NonNull Object model, int position) {
-                                    final TextView title= v.findViewById(R.id.title);
-                                    final TextView desc=v.findViewById(R.id.desc);
-                                    final ImageView image=v.findViewById(R.id.imageView);
+                                protected void populateView(@NonNull View v, @NonNull Object model, int position) {
+                                    final TextView title = v.findViewById(R.id.title);
+                                    final TextView desc = v.findViewById(R.id.desc);
+                                    final ImageView image = v.findViewById(R.id.imageView);
 
                                     query.child("gout").child("suggested").child("fat").child("fruits").addValueEventListener(new ValueEventListener() {
                                         @Override
                                         public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                                            if(dataSnapshot.getValue()!=null){
+                                            if (dataSnapshot.getValue() != null) {
 
-                                                foodconst fc=dataSnapshot.getValue(foodconst.class);
-                                                title.setText("Title:"+fc.getTitle());
-                                                desc.setText("Decsription:"+fc.getDesc());
+                                                foodconst fc = dataSnapshot.getValue(foodconst.class);
+                                                title.setText("Title:" + fc.getTitle());
+                                                desc.setText("Decsription:" + fc.getDesc());
                                                 Picasso.get().load(fc.getImage()).into(image);
 
                                             }
@@ -133,56 +139,103 @@ public class test extends AppCompatActivity {
                                         public void onCancelled(@NonNull DatabaseError databaseError) {
 
                                         }
-                                    });lv.setAdapter(adapter);
+                                    });
+                                    lv.setAdapter(adapter);
                                 }
                             };
                         }
 
                     }
                 });
-                AlertDialog alertDialog=alertDialogBuilder.create();
+                AlertDialog alertDialog = alertDialogBuilder.create();
                 alertDialog.setCanceledOnTouchOutside(true);
                 alertDialog.show();
 
             }
         });
+        final ArrayList<foodconst> snapshotListData =  new ArrayList<>();
 
-        lv=(ListView)findViewById(R.id.listView);
 
-        final FirebaseListOptions<foodconst>options=new FirebaseListOptions.Builder<foodconst>()
+        FirebaseDatabase.getInstance().getReference().child("gout").child("suggested").addValueEventListener(new ValueEventListener(){
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot){
+                if(dataSnapshot.exists()){
+                    for(DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()){
+
+                        if(dataSnapshot1.exists()){
+                            for(DataSnapshot dataSnapshot2 : dataSnapshot1.getChildren()){
+                                if (dataSnapshot2.exists()) {
+                                    foodconst fc = dataSnapshot2.getValue(foodconst.class);
+                                    snapshotListData.add(fc);
+                                    adapterFood.notifyDataSetChanged();
+
+                                }
+                            }
+                        }
+                    }
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+
+
+        });
+
+        lv = (ListView) findViewById(R.id.listView);
+
+        //Log.d("test",snapshotListData.toString());
+
+
+
+
+        /*query =  FirebaseDatabase.getInstance().getReference().child("gout").child("suggested").child("fat");
+
+        final FirebaseListOptions<foodconst> options = new FirebaseListOptions.Builder<foodconst>()
                 .setLayout(R.layout.fooddetails)
-                .setQuery(query,foodconst.class)
+                .setQuery(query, foodconst.class)
                 .build();
 
-        adapter=new FirebaseListAdapter (options) {
+        adapter = new FirebaseListAdapter(options) {
             @Override
-            protected void populateView(@NonNull  View v, @NonNull Object model, int position) {
-                final TextView title= v.findViewById(R.id.title);
-                final TextView desc=v.findViewById(R.id.desc);
-                final ImageView image=v.findViewById(R.id.imageView);
+            protected void populateView(@NonNull View v, @NonNull Object model, int position) {
+                final TextView title = v.findViewById(R.id.title);
+                final TextView desc = v.findViewById(R.id.desc);
+                final ImageView image = v.findViewById(R.id.imageView);
 
-                query.child("gout").child("suggested").addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        if(dataSnapshot.getValue()!=null){
+                foodconst goutModel = (foodconst)model;
 
-                            foodconst fc=dataSnapshot.getValue(foodconst.class);
-                            title.setText("Title:"+fc.getTitle());
-                            desc.setText("Decsription:"+fc.getDesc());
-                            Picasso.get().load(fc.getImage()).into(image);
+                for(int i=0;i<goutModel.getFoods().size();i++){
+                    foodconst fc = (foodconst)goutModel.getFood(i);
+                    title.setText("Title:" + fc.getTitle());
+                    desc.setText("Decsription:" + fc.getDesc());
+                    Picasso.get().load(fc.getImage()).into(image);
+                }
 
-                        }
-
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                    }
-                });lv.setAdapter(adapter);
             }
-        };
 
+        };*/
+
+        adapterFood = new FoodAdapter(this,0,snapshotListData);
+        lv.setAdapter(adapterFood);
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        adapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
 
     }
 }
